6cc8ac99937442acc22f76480b51063d5ee6c468
wanlinlin <leo12345639@gmail.com>
2018-05-20 11:29:10 +0800

    Remove lyric route

--- src/app.js
+++ src/app.js
@@ -6,8 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var lyric = require('./routes/lyric');
-// var users = require('./routes/users');
 
 var app = express();
 
@@ -25,8 +23,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'bower_components')));
 
 app.use('/', index);
-app.use('/lyric', lyric);
-// app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
