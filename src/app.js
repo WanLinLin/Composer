425f509f00a5bd9901f51a5cc69526f7e56d6994
wanlinlin <leo12345639@gmail.com>
2018-06-17 09:52:48 +0800

    Add back lyric route

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
+const lyric = require('./routes/lyric');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.static(path.join(__dirname, '../bower_components')));
 
 app.use('/', index);
+app.use('/lyric', lyric);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
