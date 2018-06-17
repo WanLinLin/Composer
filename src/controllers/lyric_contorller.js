e0ad3cf73dbba2e178d42f585e6872ca5ec07bb3
wanlinlin <leo12345639@gmail.com>
2018-06-17 21:20:40 +0800

    Refactor recommendLyrics to use native promise instead of async lib

--- src/controllers/lyric_contorller.js
+++ src/controllers/lyric_contorller.js
@@ -1,19 +1,17 @@
 const f = require('util').format;
-const assert = require('assert');
 const nodejieba = require('nodejieba');
 const mongoose = require('mongoose');
-const async = require('async');
 
 // Models
 const Word = require('../models/word');
 const Relation = require('../models/relation');
 
 // DB configuration
-let db;
-let user = encodeURIComponent('Leo');
-let password = encodeURIComponent('Leo@composer');
-let authMechanism = "DEFAULT";
-let url = f("mongodb://%s:%s@localhost:27017/composer?authMechanism=%s", user, password, authMechanism);
+const db;
+const user = encodeURIComponent('Leo');
+const password = encodeURIComponent('Leo@composer');
+const authMechanism = "DEFAULT";
+const url = f("mongodb://%s:%s@localhost:27017/composer?authMechanism=%s", user, password, authMechanism);
 
 // connect to mongodb
 mongoose.connect(url);
@@ -25,51 +23,31 @@ nodejieba.load({ userDict: 'assets/dict.txt.big.txt' });
 
 exports.recommendLyrics = function(req, res) {
   // cut the sentence
-  let words = nodejieba.cut(req.body.inputSentence);
-  console.log(words);
-  // the last word
-  let tWord = words.pop();
-  let rhyme = req.body.rhyme;
-  let num = req.body.wordNum;
-
-  let fWordIds;
-  let tWordId;
-  let nrWordIds;
-
-  async.waterfall([
+  const words = nodejieba.cut(req.body.inputSentence)
+  console.log(words)
+  const tailWord = words.pop()
+  const rhyme = req.body.rhyme
+  const wordNum = req.body.wordNum
+  
+  Promise.all([
     // find the tail word Id
-    function(callback) {
-      Word.getIdByWord(tWord, function(res) {
-        tWordId = res;
-        callback(null);
-      });
-    },
-    // find the front words ids
-    function(callback) {
-      Word.getIdsByWords(words, function(res) {
-        fWordIds = res;
-        callback(null);
-      });
-    },
+    Word.getIdByWord(tailWord),
     // get the num and rhyme qualified words ids
-    function(callback) { 
-      Word.getIdsByNR(num, rhyme, function(res) {
-        nrWordIds = res;
-        callback(null);
-      });
-    },
-    // get num rhyme qualified related words ordered by link and distance
-    function(callback) {
-      Relation.findByFwTws(tWordId, nrWordIds, function(res) {
-        callback(null, res);
-      });
-    },
-    // get rec words which exist in the same sentense with front words
-    function(nrlRels, callback) {
-      Relation.getCounts(fWordIds, nrlRels, function(res) {
-
+    Word.getIdsByNR(wordNum, rhyme),
+  ])
+  .then(([tailWordId, numRhymeWordsIds]) =>
+    Promise.all([
+      // find the front words ids
+      Word.getIdsByWords(words),
+      // get num rhyme qualified related words ordered by link and distance
+      Relation.findByFwTws(tailWordId, numRhymeWordsIds),
+    ])
+    .then(([frontWordsIds, numRhymeRelations]) =>
+      Promise.all([
+        // get rec words which exist in the same sentense with front words
+        Relation.getCounts(frontWordsIds, numRhymeRelations)
         // sort by link, then distance, then count
-        res.sort((a, b) => {
+        .then(res => res.sort((a, b) => {
           if(a.link > b.link) return -1;
           if(a.link < b.link) return 1;
           if(a.distance > b.distance) return -1;
@@ -78,50 +56,38 @@ exports.recommendLyrics = function(req, res) {
           if(a.count < b.count) return 1;
 
           return 0;
-        });
-        callback(null, res);
-      });
-    },
+        })),
+        numRhymeWordsIds,
+      ])
+    )
+  )
+  .then(([recs, numRhymeWordsIds]) => {
     // recommend nr words if there's no relation to the sentence
-    function(recs, callback) {
-      if (recs.length < 20) {
-        let num = 20 - recs.length;
+    if (recs.length < 20) {
+      const num = 20 - recs.length;
 
-        // random nr words
-        nrWordIds.sort(function() {
-          return 0.5 - Math.random();
-        });
+      // random nr words
+      numRhymeWordsIds.sort(function() {
+        return 0.5 - Math.random();
+      });
 
-        Word.find({
-          _id: { $in: nrWordIds.slice(0, num) }
-        })
-        .exec(function (err, res) {
-          assert.equal(null, err);
-          callback(null, {recs: recs, nr: res});
-        });
-      }
-      else {
-        callback(null, {recs: recs});
-      }
+      return Word.find({
+        _id: { $in: numRhymeWordsIds.slice(0, num) }
+      })
+      .exec()
+      .then(res => ({
+        recs,
+        nrs: res,
+      }))
     }
-    ],
-    function (err, result) {
-      assert.equal(null, err);
-      let words = [];
-
-      for (let i = 0; i < result.recs.length; i++) {
-        words.push(result.recs[i].t_word[0].word);
-      }
-
-      if (result.nr) {
-        for (let i = 0; i < result.nr.length; i++) {
-          words.push(result.nr[i].word);
-        }
-      }
-
-      res.json({words: words});
-    }
-  );
-
+    
+    return {recs}
+  })
+  .then(({recs = [], nrs = []}) => 
+    res.json({words: [
+      ...recs.map(rec => rec.t_word[0].word),
+      ...nrs.map(nr => nr.word),
+    ]})
+  )
 };
 
